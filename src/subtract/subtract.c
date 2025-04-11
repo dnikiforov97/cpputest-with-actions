a4c96194cd779903e7a9deff0be079d387e37515
dnikiforov97 <david@finovatechco.com>
2025-04-10 19:02:55 -0700

    added subtraction with unit tests

---
+++ src/subtract/subtract.c
@@ -0,0 +1,6 @@
+#include "subtract.h"
+
+float subtract(float firstValue, float secondValue)
+{
+    return firstValue - secondValue;
+}
---
+++ tests/subtract/test.cpp
@@ -0,0 +1,43 @@
+#include <CppUTest/TestHarness.h>
+
+extern "C" {
+#include "subtract/subtract.h"
+}
+
+// Create a test group
+TEST_GROUP(average_test_group)
+{
+    void setup() override
+    {
+        // This function is called before each test
+    }
+
+    void teardown() override
+    {
+        // This function is called after each test
+    }
+};
+
+TEST(average_test_group, first_test)
+{
+    float sub= subtract(5, 2);
+    CHECK_EQUAL(3, sub);
+}
+
+TEST(average_test_group, get_a_negative_num_test)
+{
+    float sub= subtract(2, 5);
+    CHECK_EQUAL(-3, sub);
+}
+
+TEST(average_test_group, passing_a_zero_test)
+{
+    float sub= subtract(0, 5);
+    CHECK_EQUAL(-5, sub);
+}
+
+TEST(average_test_group, sub_by_zero_test)
+{
+    float sub= subtract(5, 0);
+    CHECK_EQUAL(5, sub);
+}
